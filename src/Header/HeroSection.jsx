47f88520f3b49c5fed975507cf41f61d6921c511
tofae <arnobtuhin,j@gmail.com>
2023-09-28 00:07:36 +0600

    lorem added

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -7,6 +7,7 @@ const HeroSection = () => {
             <h1>area</h1>
             <h1>areafgjfgj</h1>
             <h1>sdfjkghsdfjkghsdjkfgh</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sint officiis! Facilis, ullam officia praesentium doloribus illo ut. Minus, assumenda?</p>
         </div>
     );
 };
