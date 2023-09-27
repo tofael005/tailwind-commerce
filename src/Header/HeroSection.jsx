3a96608822c46688ae2b11948b268fcb1e68cb39
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-28 00:15:05 +0600

    peragraph

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -8,6 +8,7 @@ const HeroSection = () => {
             <h1>areafgjfgj</h1>
             <h1>sdfjkghsdfjkghsdjkfgh</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sint officiis! Facilis, ullam officia praesentium doloribus illo ut. Minus, assumenda?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, at.</p>
         </div>
     );
 };
