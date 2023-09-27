4322575879620d0bbc1ea2b1b95aaeff5841e37c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-28 00:33:39 +0600

    world

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -4,11 +4,7 @@ const HeroSection = () => {
     return (
         <div className="mt-6">
             <h1>Hero Area</h1>
-            <h1>area</h1>
-            <h1>areafgjfgj</h1>
-            <h1>sdfjkghsdfjkghsdjkfgh</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, sint officiis! Facilis, ullam officia praesentium doloribus illo ut. Minus, assumenda?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, at.</p>
+           <h1>Hello World</h1>
         </div>
     );
 };
