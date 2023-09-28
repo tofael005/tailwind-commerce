00f64aee0f95557a403593f9c2d1d8fbe409edb1
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-29 00:03:25 +0600

    worlds added

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -5,6 +5,7 @@ const HeroSection = () => {
         <div className="mt-6">
             <h1>Hero Area</h1>
            <h1>Hello World</h1>
+           <h1>Hello Worlds</h1>
         </div>
     );
 };
