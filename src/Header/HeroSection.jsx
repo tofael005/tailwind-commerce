8bcd3b8d6dc85780f2b54496a99419aedb89daf1
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-30 22:33:05 +0600

    another image

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -3,9 +3,13 @@
 const HeroSection = () => {
     return (
         <div className="mt-6">
-            <div>
+            <div className="grid md:grid-cols-2 gap-6">
                 <div>
-                    <img src="/src/assets/product/ideogram (11).jpeg" alt="" />
+                    <img className="w-[800px] h-[600px] object-cover" src="/src/assets/product/ideogram (11).jpeg" alt="" />
+                </div>
+                <div className="w-[400px] grid grid-cols-2 gap-6">
+                    <img src="/src/assets/product/ideogram (12).jpeg" alt="" />
+                    <img src="/src/assets/product/ideogram (13).jpeg" alt="" />
                 </div>
             </div>
         </div>
