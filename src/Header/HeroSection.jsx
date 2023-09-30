9817a6da6276db2062318a76a44c2c3016a01f18
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-30 22:52:21 +0600

    product details

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -4,9 +4,9 @@ const HeroSection = () => {
     return (
         <div className="mt-6">
             <div className="max-w-[1240px] md:flex gap-6 mx-auto px-3">
-                <div className="md:w-[800px] mb-6">
-                    <img className="md:w-full h-[600px] object-cover relative" src="/src/assets/product/ideogram (11).jpeg" alt="" />
-                    <button >Click</button>
+                <div className="md:w-[800px] mb-6 relative">
+                    <img className="md:w-full h-[600px] object-cover" src="/src/assets/product/ideogram (11).jpeg" alt="" />
+                    <button className="absolute top-[500px] left-32 bg-orange-400 p-3">Click Details the food</button>
                 </div>
                 <div className="md:w-[400px]">
                     <img className="w-full h-[270px] md:mb-14 mb-6" src="/src/assets/product/ideogram (12).jpeg" alt="" />
