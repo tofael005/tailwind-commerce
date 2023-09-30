4d2f96dbee15eac6ee732a5fba0d58117de3e7e9
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-09-30 22:47:16 +0600

    btn added

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -3,13 +3,14 @@
 const HeroSection = () => {
     return (
         <div className="mt-6">
-            <div className="max-w-[1240px] md:flex gap-6 mx-auto">
+            <div className="max-w-[1240px] md:flex gap-6 mx-auto px-3">
                 <div className="md:w-[800px] mb-6">
-                    <img className="md:w-full h-[600px] object-cover" src="/src/assets/product/ideogram (11).jpeg" alt="" />
+                    <img className="md:w-full h-[600px] object-cover relative" src="/src/assets/product/ideogram (11).jpeg" alt="" />
+                    <button >Click</button>
                 </div>
                 <div className="md:w-[400px]">
-                    <img className="w-full h-[250px] md:mb-16 mb-6" src="/src/assets/product/ideogram (12).jpeg" alt="" />
-                    <img className="w-full h-[250px]" src="/src/assets/product/ideogram (13).jpeg" alt="" />
+                    <img className="w-full h-[270px] md:mb-14 mb-6" src="/src/assets/product/ideogram (12).jpeg" alt="" />
+                    <img className="w-full h-[270px] md:mt-3" src="/src/assets/product/ideogram (13).jpeg" alt="" />
                 </div>
             </div>
         </div>
