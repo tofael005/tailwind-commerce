ce7ec7f7bb85e1ecb01f8c4dc739ee9c33bafc18
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-05 00:50:40 +0600

    hero addeds

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -15,6 +15,7 @@ const HeroSection = () => {
                     <h1>hello</h1>
                     <h1>hello</h1>
                     <h1>hello</h1>
+                    <h1>hello</h1>
                 </div>
             </div>
         </div>
