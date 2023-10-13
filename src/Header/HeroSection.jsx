7ffd4478020e687b174b54095914402b9f3eca6d
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-10-14 00:53:22 +0600

    items teo

--- src/Header/HeroSection.jsx
+++ src/Header/HeroSection.jsx
@@ -9,14 +9,19 @@ const HeroSection = () => {
                     <button className="absolute top-[500px] md:left-80 left-24 bg-sky-400 font-semibold hover:text-white rounded-md hover:shadow-black/30 hover:shadow-md duration-300 p-3">Click the food details.</button>
                 </div>
                 <div className="md:w-[400px]">
-                    <div>
-                        <img className="w-full h-[270px] rounded-md mb-2" src="/src/assets/product/ideogram (12).jpeg" alt="" />
-                        <div className="flex justify-between items-center">
-                            <p>American Burger</p>
-                            <p>$24</p>
+                    <div className="hover:shadow hover:shadow-black/30 duration-300 rounded-md">
+                        <img className="w-full h-[270px] rounded-md mb-1" src="/src/assets/product/ideogram (12).jpeg" alt="" />
+                        <div className="flex justify-between items-center p-2">
+                            <p className="font-bold">American Burger</p>
+                            <p className="text-white font-extrabold px-3 bg-blue-500 rounded-md">$24</p>
                         </div>
                     </div>
-                    <img className="w-full h-[270px] rounded-md md:mt-3" src="/src/assets/product/ideogram (13).jpeg" alt="" />
+                    <div className="hover:shadow hover:shadow-black/30 duration-300 rounded-md">
+                        <img className="w-full h-[270px] rounded-md md:mt-3" src="/src/assets/product/ideogram (13).jpeg" alt="" />
+                        <div className="flex justify-between items-center p-2">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
